c22c3bdaf693c105d798ca88d6a7eeb153be6fb9
Daniel Rose <daniel-rose@gmx.de>
2019-07-12 17:01:39 +0200

    - better performance - better error handling

--- src/FondOfSpryker/Zed/PriceListApi/Business/Model/PriceListApi.php
+++ src/FondOfSpryker/Zed/PriceListApi/Business/Model/PriceListApi.php
@@ -13,6 +13,7 @@ use Generated\Shared\Transfer\PriceListApiTransfer;
 use Generated\Shared\Transfer\PriceListTransfer;
 use Generated\Shared\Transfer\PriceProductTransfer;
 use Propel\Runtime\Connection\ConnectionInterface;
+use Spryker\Shared\Log\LoggerTrait;
 use Spryker\Zed\Api\ApiConfig;
 use Spryker\Zed\Api\Business\Exception\EntityNotFoundException;
 use Spryker\Zed\Api\Business\Exception\EntityNotSavedException;
@@ -20,6 +21,8 @@ use Throwable;
 
 class PriceListApi implements PriceListApiInterface
 {
+    use LoggerTrait;
+
     /**
      * @var \FondOfSpryker\Zed\PriceListApi\Dependency\Facade\PriceListApiToPriceListFacadeInterface
      */
@@ -117,8 +120,8 @@ class PriceListApi implements PriceListApiInterface
      * @param \Generated\Shared\Transfer\PriceListTransfer $priceListTransfer
      * @param \Generated\Shared\Transfer\PriceListApiTransfer $priceListApiTransfer
      *
+     * @throws
      * @throws \Spryker\Zed\Api\Business\Exception\EntityNotSavedException
-     * @throws \FondOfSpryker\Zed\PriceListApi\Business\Exception\MissingPriceDimensionException
      *
      * @return \Generated\Shared\Transfer\ApiItemTransfer
      */
@@ -126,40 +129,71 @@ class PriceListApi implements PriceListApiInterface
         PriceListTransfer $priceListTransfer,
         PriceListApiTransfer $priceListApiTransfer
     ): ApiItemTransfer {
+        $priceListTransfer = $this->persistPriceList($priceListTransfer);
+
         $this->connection->beginTransaction();
 
-        try {
-            $priceListTransfer = $this->priceListFacade->persistPriceList($priceListTransfer);
-
-            foreach ($priceListApiTransfer->getPriceListEntries() as $priceProductTransfer) {
-                if ($priceProductTransfer->getPriceDimension() === null) {
-                    throw new MissingPriceDimensionException('Price dimension is missing.', ApiConfig::HTTP_CODE_INTERNAL_ERROR);
-                }
-
-                $priceProductTransfer->getPriceDimension()->setIdPriceList($priceListTransfer->getIdPriceList());
-
-                $this->persistPriceProductTransfer($priceProductTransfer);
+        foreach ($priceListApiTransfer->getPriceListEntries() as $priceProductTransfer) {
+            try {
+                $this->persistPriceProductTransfer($priceListTransfer, $priceProductTransfer);
+            } catch (Throwable $throwable) {
+                $this->connection->rollBack();
+                throw new EntityNotSavedException(sprintf('Could not save price list entries: %s', $throwable->getMessage()), ApiConfig::HTTP_CODE_INTERNAL_ERROR);
             }
-
-            $this->connection->commit();
-        } catch (Throwable $throwable) {
-            $this->connection->rollBack();
-            throw new EntityNotSavedException(sprintf('Could not save price list: %s', $throwable->getMessage()), ApiConfig::HTTP_CODE_INTERNAL_ERROR);
         }
+
+        $this->connection->commit();
 
         return $this->apiQueryContainer->createApiItem($priceListApiTransfer, $priceListTransfer->getIdPriceList());
     }
 
     /**
+     * @param \Generated\Shared\Transfer\PriceListTransfer $priceListTransfer
+     *
+     * @throws \Spryker\Zed\Api\Business\Exception\EntityNotSavedException
+     *
+     * @return \Generated\Shared\Transfer\PriceListTransfer
+     */
+    protected function persistPriceList(PriceListTransfer $priceListTransfer): PriceListTransfer
+    {
+        $this->connection->beginTransaction();
+
+        try {
+            $priceListTransfer = $this->priceListFacade->persistPriceList($priceListTransfer);
+        } catch (Throwable $throwable) {
+            $this->connection->rollBack();
+            throw new EntityNotSavedException(sprintf('Could not save price list: %s', $throwable->getMessage()), ApiConfig::HTTP_CODE_INTERNAL_ERROR);
+        }
+
+        $this->connection->commit();
+
+        return $priceListTransfer;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\PriceListTransfer $priceListTransfer
      * @param \Generated\Shared\Transfer\PriceProductTransfer $priceProductTransfer
+     *
+     * @throws \FondOfSpryker\Zed\PriceListApi\Business\Exception\MissingPriceDimensionException
      *
      * @return \Generated\Shared\Transfer\PriceProductTransfer
      */
     protected function persistPriceProductTransfer(
+        PriceListTransfer $priceListTransfer,
         PriceProductTransfer $priceProductTransfer
     ): PriceProductTransfer {
+        if ($priceProductTransfer->getPriceDimension() === null) {
+            throw new MissingPriceDimensionException('Price dimension is missing.', ApiConfig::HTTP_CODE_INTERNAL_ERROR);
+        }
+
+        $priceProductTransfer->getPriceDimension()->setIdPriceList($priceListTransfer->getIdPriceList());
+
         foreach ($this->priceProductHydrationPlugins as $priceProductHydrationPlugin) {
             $priceProductTransfer = $priceProductHydrationPlugin->hydrate($priceProductTransfer);
+        }
+
+        if ($priceProductTransfer->getIdProductAbstract() === null && $priceProductTransfer->getIdProduct() === null) {
+            return $priceProductTransfer;
         }
 
         $this->priceProductPriceListFacade->savePriceProductPriceList($priceProductTransfer);

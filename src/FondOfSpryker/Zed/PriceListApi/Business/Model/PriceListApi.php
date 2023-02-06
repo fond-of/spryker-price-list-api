7a1d43d7d3dd5cdd7f4a4ec7c7600fe1ebcdd49c
Jozsef Geng <jozsef.geng@fondof.de>
2023-02-06 14:47:52 +0100

    fix

--- src/FondOfSpryker/Zed/PriceListApi/Business/Model/PriceListApi.php
+++ src/FondOfSpryker/Zed/PriceListApi/Business/Model/PriceListApi.php
@@ -283,6 +283,10 @@ class PriceListApi implements PriceListApiInterface
         $query = $this->buildQuery($apiRequestTransfer);
         $collection = $this->transferMapper->toTransferCollection($query->find()->toArray());
 
+        foreach ($collection as $k => $priceListApiTransfer) {
+            $collection[$k] = $this->get($priceListApiTransfer->getIdPriceList());
+        }
+
         $apiCollectionTransfer = $this->apiFacade->createApiCollection($collection);
         $apiCollectionTransfer = $this->addPagination($query, $apiCollectionTransfer, $apiRequestTransfer);
 
